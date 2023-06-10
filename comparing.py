5fdd9040eed3d978ab1150cd0f9b4cb239cd7c2e
Vadim Ryabov <mr.holeyko@gmail.com>
2023-06-10 17:55:15 +0300

    fix gauss-newton, dogleg + tests

--- comparing.py
+++ comparing.py
@@ -2,8 +2,8 @@ import random
 
 import numpy as np
 
-from linreg import sgd_handler, Methods, visualise_linear, gen_linear_reg
-from main import dogleg
+from linreg import sgd_handler, Methods, visualise_linear, gen_linear_reg, poly, poly_array, LinearRegression
+from main import dogleg, gauss_newton, bfgs
 
 
 def refresh_linreg(linreg, start_x):
@@ -11,21 +11,47 @@ def refresh_linreg(linreg, start_x):
     linreg.W_points = [np.copy(linreg.W)]
 
 
-def comparing():
-    start_x = np.array([30., 30.])
-    linregs_examples = [gen_linear_reg(1, random.randint(10, 50), -5., 5., -10., 10., 1.) for _ in range(6)]
+def comparing(linreg_func, title, visualize_prev_methods=True):
+    start_points = [np.array([float(random.randint(10, 40)), float(random.randint(10, 50))]) for _ in range(10)]
+    linregs_examples = [gen_linear_reg(1, random.randint(10, 100), -5., 5., -10., 10., 1.) for _ in range(6)]
     lr = lambda *args: 0.01
 
     for linreg in linregs_examples:
+        start_x = start_points[random.randint(0, len(start_points) - 1)]
         refresh_linreg(linreg, start_x)
-        dogleg_points = dogleg(linreg.loss, linreg.W, store_points=True)
-        visualise_linear(linreg.loss, dogleg_points, "Dogleg", "w_1", "w_2")
-        for method in Methods:
-            refresh_linreg(linreg, start_x)
-            sgd_handler(linreg, lr, method, store_points=True)
-            sgd_points = linreg.W_points
-            visualise_linear(linreg.loss, sgd_points, method.name, "w_1", "w_2")
+        points = linreg_func(np.copy(start_x), linreg)
+        visualise_linear(linreg.loss, points, title, "w_1", "w_2")
+
+        if visualize_prev_methods:
+            for method in Methods:
+                refresh_linreg(linreg, start_x)
+                sgd_handler(linreg, lr, method, store_points=True)
+                sgd_points = linreg.W_points
+                visualise_linear(linreg.loss, sgd_points, method.name, "w_1", "w_2")
 
 
+def gauss_newton_comparing():
+    def calc_points(start_x, linreg):
+        funcs = np.array([lambda W, i=i: np.dot(linreg.T[i], W) - linreg.Y[i] for i in range(len(linreg.X))])
+        return gauss_newton(start_x, funcs, store_points=True)
 
-comparing()
+    comparing(calc_points, "Gauss-Newton", visualize_prev_methods=False)
+
+
+def dogleg_comparing():
+    def calc_points(start_x, linreg):
+        return dogleg(linreg.loss, start_x, store_points=True)
+
+    comparing(calc_points, "Dogleg", visualize_prev_methods=False)
+
+
+def bfgs_comparing():
+    def calc_points(start_x, linreg):
+        return bfgs(linreg.loss, start_x, store_points=True)
+
+    comparing(calc_points, "BFGS", visualize_prev_methods=False)
+
+
+gauss_newton_comparing()
+dogleg_comparing()
+bfgs_comparing()
--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import math
 import numpy as np
 
 
-def derivative(f, x, i, delt=0.01):
-    x_c = np.copy(x)
-    x_c[i] += delt
-    first_val = f(x_c)
-    x_c[i] -= 2 * delt
-    second_val = f(x_c)
-    x_c[i] += delt
-    return (first_val - second_val) / (2 * delt)
+def derivative(f, x, i, delt=0.0001):
+    x_1 = np.copy(x)
+    x_2 = np.copy(x)
+    x_1[i] += delt
+    x_2[i] -= delt
+    y_1 = f(x_1)
+    y_2 = f(x_2)
+    return (y_1 - y_2) / (2 * delt)
 
 
 def grad(f, delt=0.01):
@@ -24,72 +24,46 @@ def grad(f, delt=0.01):
 
 
 class Jacobian:
-    # init_funcs - массив лямбд
-    # arity - int, арность функций
     def __init__(self, funcs, arity):
+        self.body = np.array([[0. for _ in range(arity)] for _ in range(len(funcs))])
         self.funcs = funcs
-        self.eps = 0.01
-        self.body = np.asarray([[0. for _ in range(arity)] for _ in range(len(funcs))])
 
     # point - точка в виде массива
-    def calc_body(self, point):
+    def calc_Jacobian(self, point):
         for i in range(len(self.funcs)):
             for j in range(len(point)):
-                prev = point[j]
-                point[j] += self.eps
-                minuend_func = self.funcs[i]
-                minuend = minuend_func(point)
-                point[j] = prev
-                subtrahend = self.funcs[i](point)
-                self.body[i][j] = (minuend - subtrahend) / self.eps
-
-    def get_transposed_body(self):
-        return np.transpose(self.body)
+                self.body[i][j] = derivative(self.funcs[i], point, j)
 
 
-# point - точка в виде массива
-# funcs - массив лямбд
-def func_wrap(point, funcs):
-    result = np.array([0. for _ in range(len(funcs))])
+def get_R(point, funcs):
+    result = []
     for i in range(len(funcs)):
-        result[i] = funcs[i](point)
-    return result
+        result.append(funcs[i](point))
+    return np.array(result)
 
 
-# start_point - точка в виде массива
-# funcs - массив лямбд
-def min_point_by_gauss_newton(start_point, funcs):
-    start_point = np.asarray(start_point)
+def gauss_newton(start_point, funcs, max_steps=10000, store_points=False):
+    points = [np.copy(start_point)]
+    start_point = np.array(start_point)
+    x_prev = start_point
     J = Jacobian(funcs, len(start_point))
-    for i in range(10000):
-        J.calc_body(start_point)
-        if i % 100 == 0: print(start_point)
-        # tmp_matrix = J.get_transposed_body() @ J.get_body()
-        diff = np.asarray(start_point) - np.asarray(
-            np.linalg.pinv(J.body) @ func_wrap(start_point, funcs))
-        start_point = diff
+    f = lambda x: sum([funcs[k](x) ** 2 for k in range(len(funcs))]) / 2
+    for i in range(max_steps):
+        J.calc_Jacobian(start_point)
+        p = - np.linalg.inv(J.body.T @ J.body) @ J.body.T @ get_R(start_point, funcs)
+        alpha = dichotomy(lambda a: f(start_point + a * p), 0., right_border_calc(lambda a: f(start_point + a * p)))
+        start_point = start_point + alpha * p
+        if np.linalg.norm(start_point - x_prev) < 0.00001:
+            break
+        x_prev = start_point
 
+        if store_points:
+            points.append(np.copy(start_point))
 
-def func1(args):
-    return args[0]
-
-
-def func2(args):
-    return args[0] ** 2
-
-
-def func3(args):
-    return math.cos(args[0]) + 1.0
-
-
-# min_point_by_gauss_newton([1090990.9090], [func1, func2])
-# print('\n')
-# min_point_by_gauss_newton([919.0], [func3])
-
-# print([[1., 2.], [0., 0.]] @ np.linalg.pinv([[1., 2.], [0., 0.]]))
-
-# =======================================================DOG=LEG=========================================================
-# NOTE: придумать что делать если гессиан не положительно определенный
+    if store_points:
+        return points
+    else:
+        return [start_point, f(start_point)]
 
 def hessian(f):
     def calc(x):
@@ -102,9 +76,9 @@ def hessian(f):
     return calc
 
 
-def min_point_by_trust_region_func(f, x_k, get_p, recalc_m, delta=1., delta_max=10., eps=0.001, eta=0.2,
-                                   max_steps=1000, store_points=False):
-    div_eps = 0.0000000000000000000000000000000000001
+def trust_region(f, x_k, get_p, recalc_m, delta=1., delta_max=10., eps=0.001, eta=0.2,
+                 max_steps=1000, store_points=False):
+    div_eps = 0.00001
     g = grad(f)
     B = hessian(f)
     points = [np.copy(x_k)]
@@ -125,109 +99,62 @@ def min_point_by_trust_region_func(f, x_k, get_p, recalc_m, delta=1., delta_max=
             if store_points:
                 points.append(np.copy(x_k))
 
-    return points
+    if store_points:
+        return points
+    else:
+        return x_k
 
 
-def recalc_m_for_dogleg(f, g, B, x):
-    return lambda p: f(x) + g(x).T @ p + 0.5 * p.T @ B(x) @ p
+def m_for_dogleg(f, g, B, x_k):
+    return lambda p: f(x_k) + g(x_k).T @ p + 0.5 * p.T @ B(x_k) @ p
 
 
-def get_p_by_dogleg(f, g, B, x_k, delta):
-    g_comp = g(x_k)
-    B_comp = B(x_k)
-    p_b = -np.linalg.inv(B_comp) @ g_comp
+def p_by_dogleg(f, g, B, x_k, delta_k):
+    grad_k = g(x_k)
+    B_k = B(x_k)
+    p_b = -np.linalg.inv(B_k) @ grad_k
 
-    if np.linalg.norm(p_b) <= delta:
+    if np.linalg.norm(p_b) <= delta_k:
         return p_b
 
-    p_u = -((g_comp.T @ g_comp) / (g_comp.T @ B_comp @ g_comp)) * g_comp
+    p_u = -((grad_k.T @ grad_k) / (grad_k.T @ B_k @ grad_k)) * grad_k
 
-    if np.linalg.norm(p_u) <= delta:
+    if np.linalg.norm(p_u) <= delta_k:
         p_delt = p_b - p_u
-        return p_delt * (delta / np.linalg.norm(p_delt))
+        a = np.linalg.norm(p_delt)
+        b = 2 * p_delt.T @ p_b
+        c = np.linalg.norm(p_b)
+        D = (b ** 2 - 4 * a * c)
+        if D < 0:
+            D = 0
+        sqrt_d = math.sqrt(D)
+        alpha = (-b + sqrt_d) / (2 * a)
+        return p_u + (p_delt / np.linalg.norm(p_delt)) * alpha
     else:
-        return -(g_comp * (delta / np.linalg.norm(g_comp)))
+        return -(delta_k * (grad_k / np.linalg.norm(grad_k)))
 
 
 def dogleg(f, x_k, delta=1., delta_max=10., eps=0.001, eta=0.2, max_steps=1000, store_points=False):
-    return min_point_by_trust_region_func(f, x_k, get_p_by_dogleg, recalc_m_for_dogleg, delta, delta_max, eps, eta,
-                                          max_steps, store_points)
+    return trust_region(f, x_k, p_by_dogleg, m_for_dogleg, delta, delta_max, eps, eta,
+                        max_steps, store_points)
 
-
-# print(min_point_by_trust_region_func(
-#     lambda ab: sum([(ab[0] * np.sin(i[0]) + ab[1] * np.cos(i[0]) - i[1]) ** 2 for i in
-#                     [[-1.8769384497209494, 0.3208500555232947], [-1.5192895330096263, 0.7772238982741027],
-#                      [-6.608540870385862, 0.28694079206001677], [6.914561957007351, -0.06537633613384441],
-#                      [8.625076710401945, -0.5439463209772875], [5.1481282311186085, 0.2954095885701955],
-#                      [9.917397351785983, 0.6381880291402098], [-3.1196118783564897, 0.22963471498883092],
-#                      [7.770694338086578, -1.2223477289499438], [7.055529363737438, -1.0278035114572843],
-#                      [-2.9556342432708416, 0.4986001160756802], [-9.23375090675026, -0.11282009684632777],
-#                      [5.8102049796332, -0.06311829101291822], [-9.545091375894588, -0.2831938836416489],
-#                      [-5.826223513214055, -0.21697795539658382], [-4.267697868348746, -0.4320163598688562],
-#                      [-2.8351567506352993, -0.09527386407734567], [5.921967856445107, 0.3454482081185837],
-#                      [4.976381342007938, 0.3995511186698528], [-2.8383706094032597, 0.032189666747653],
-#                      [-0.1558717909590719, -0.3605651138568633], [8.311655199531476, -0.8862195212623891],
-#                      [3.636532932128752, 0.1657490592521948], [-1.8587270756994219, 0.26306012702413495],
-#                      [-7.003595530144828, 0.20856244222042208], [8.145120641402237, -1.1946442481763837],
-#                      [-2.6039220943613266, 0.40330638566489185], [2.030233984766891, -0.4039502977372882],
-#                      [-9.30838826455463, 0.5484281018529541], [-7.515652514258974, 0.9175554805036741],
-#                      [-8.641684557556337, 0.2487934650864125], [5.787830587963349, 0.12871259477323344],
-#                      [-3.336251538807698, -0.5607635286354037], [3.2989626372915133, 0.39796008847194314],
-#                      [8.475275481335945, -0.1898871866282117], [8.510307667083573, -0.0882847385541009],
-#                      [0.9403860630101857, -0.869380633325813], [-4.455111931533011, -0.26243599166441045],
-#                      [3.6809065421937444, 0.8459068272506574], [-7.794230889015365, 0.5593025190698748],
-#                      [8.416566787343513, -1.0135768400576146], [4.366223474432299, 0.7386057618087559],
-#                      [-6.6243137214974235, 0.6434261606159205], [-4.957818865828165, -0.6363246862790314],
-#                      [-2.3214050706217666, 0.1692868068782456], [-4.897174977451943, -0.7208018565918584],
-#                      [2.2294776894465222, -0.8908121153134009], [-7.420431574442363, 1.1435382771435183],
-#                      [9.317542826940517, 0.32359792377633667], [8.893593454955838, -0.5974341794945599],
-#                      [9.798233640129919, 0.7446134535699878], [-1.7736250254030885, 1.2349185846703152],
-#                      [8.114721599613357, -0.33309164366738186], [-4.203422879134838, -1.039643397490626],
-#                      [-5.715077994512772, -0.6199246111973729], [-5.65282123529991, -0.3865670549416059],
-#                      [-0.02618847153689252, -0.4812735346323904], [-3.0927736450612198, 0.4523686568831471],
-#                      [1.9578262661860517, -0.23184286846988866], [9.042305545556832, -0.45899493868218166],
-#                      [7.603352299789417, -0.5196427622191989], [6.462315734493561, -0.28379994231167505],
-#                      [8.800318805259355, -0.8747963357090377], [7.982956456534332, -0.5661667650894068],
-#                      [8.493633976545535, -0.6398908864225873], [-8.507453813809088, 0.22818545293343862],
-#                      [-4.099226195893689, -0.6539443934810435], [-7.094286935689331, 0.38487506670975546],
-#                      [2.1427046885173766, -0.5534282300406761], [5.333041010106152, 0.6292728704907733],
-#                      [0.853971993876435, -0.38527038899230626], [-7.599813304016814, 0.38500486127178035],
-#                      [8.074980951655238, -0.2787983527212796], [-8.401307320784529, 0.8869791593547965],
-#                      [-9.581188783561354, -0.2676071029736081], [-6.765753630338482, 0.11839890625882782],
-#                      [-5.774334189744044, -0.7909181968145023], [3.6709428631692873, 0.22794758235437784],
-#                      [-0.22985889707427987, -0.3357482577501226], [-3.896827986583693, -0.38176431125364696],
-#                      [-3.0952683519302866, -0.27630151900996536], [8.476756429943777, -0.9397625333302705],
-#                      [9.2252207202283, 0.0683457109929656], [7.553374460978816, -0.6306034006252805],
-#                      [0.06777147014958373, -0.02743905089885279], [-1.3759622857076277, 1.012823853632875],
-#                      [9.696276569505745, 0.17676875366448352], [-3.8802860593980393, -0.11476431685199606],
-#                      [-8.536713201505501, 0.6811143352038459], [-6.727458139672818, 0.30791046507181113],
-#                      [-6.1363822008460644, -0.38070205955231873], [-3.045580589524599, 0.09395640991513232],
-#                      [3.9205003475302984, 0.8971066369532453], [2.3872729089907434, -0.17871475261304282],
-#                      [1.004438213973069, -1.1464860519635782], [-2.611790306414994, 0.24870209231862977],
-#                      [6.567977983346353, -0.014017950038410776], [4.700813066523368, 0.6630580629820589],
-#                      [6.17015546377386, -0.42730277869509875], [9.79178198342726, 0.08931314018149278]]]),
-#     np.array((10., 10.)),
-#     get_p_by_dogleg, recalc_m_for_dogleg
-# ))
-#
-# print(hessian(lambda x: (x[0] - 5.) * (x[0] - 5.) + (x[1] + 6.) * (x[1] + 6.))(np.array((10., 10.))))
 
 wolfe_cond_template = lambda c1, c2, x, func, gk: lambda a, b: not (
-        (func.value(x - ((a + b) / 2) * gk) <= (func.value(x) + c1 * ((a + b) / 2) * np.dot(gk, -gk))) and (
-        np.dot(func.grad(x - ((a + b) / 2) * gk), -gk) >= c2 * np.dot(gk, -gk)))
+        (func(x - ((a + b) / 2) * gk) <= (func(x) + c1 * ((a + b) / 2) * np.dot(gk, -gk))) and (
+        np.dot(grad(func)(x - ((a + b) / 2) * gk), -gk) >= c2 * np.dot(gk, -gk)))
 
 wolfe_cond = lambda: ""
 
 
-def dichotomy(func, a_1, a_2, eps, delt, is_wolfe=False):
+def dichotomy(func, a_1, a_2, eps=0.01, delt=0.0001, is_wolfe=False):
     cond = lambda a, b: abs(a - b) >= eps
     if is_wolfe:
         cond = wolfe_cond
     while cond(a_1, a_2):
         new_a_1 = (a_1 + a_2) / 2 - delt
         new_a_2 = (a_1 + a_2) / 2 + delt
-        fv1 = func.value(new_a_1)
-        fv2 = func.value(new_a_2)
+        fv1 = func(new_a_1)
+        fv2 = func(new_a_2)
         if fv2 > fv1:
             a_2 = new_a_2
         elif fv2 < fv1:
@@ -240,26 +167,48 @@ def dichotomy(func, a_1, a_2, eps, delt, is_wolfe=False):
 
 def right_border_calc(func):
     right_start = 0.0000001
-    zero = func.value(0)
-    while zero >= func.value(right_start):
-        right_start *= 2
+    zero = func(0)
+    while zero >= func(right_start):
+        right_start *= 1.2
 
     return right_start
 
 
-def calc_min_by_bfgs(x, f):
-    I = np.eye(len(x))
-    H = I
-    grad_prev = grad(f)(x)
-    p = -H * grad_prev
-    x_prev = x
-    x += dichotomy(f, 0, r) * p
-    # в ск засунуть разность нового х и переданного
-    # в ук засунуть разность градиента при новом х и при старом           также сохранить градиент и икс текущие
+def bfgs(f, x, breaking_eps=0.0001, store_points=False):
+    global wolfe_cond
+    points = [np.copy(x)]
+    H = np.eye(len(x))
+    grad_prev = np.array(grad(f)(x))
+    p = np.array(-H @ grad_prev)
+    x_prev = np.copy(x)
+    eps = 0.01
+    delt = 0.0001
+    wolfe_cond = wolfe_cond_template(0.001, 0.999, x_prev, f, grad_prev)
+    alpha = dichotomy(lambda a: f(x + a * p), 0., right_border_calc(lambda a: f(x + a * p)),
+                      eps, delt,
+                      is_wolfe=True)
+    x += alpha * p
     for i in range(10000):
-        y_k = grad(f)(x) - grad(f)(x_prev)
-        s_k = x - x_prev
-        ro_k = 1 / (y_k.T * s_k)
-        H = (I - ro_k @ s_k @ y_k.T) @ H @ (I - ro_k @ y_k @ s_k.T) + ro_k @ s_k @ s_k.T
-        p = -H @ grad(f)(x)
-        # x -= alpa()
+        # print(x)
+        grad_tmp = np.array(grad(f)(x))
+        y_k = np.array(grad_tmp - grad_prev)
+        s_k = np.array(x - x_prev)
+        ro_k = 1 / (y_k.T @ s_k)
+        H = (np.eye(len(x)) - ro_k * s_k @ y_k.T) @ H @ (np.eye(len(x)) - ro_k * y_k @ s_k.T) + ro_k * s_k @ s_k.T
+        p = np.array(-H @ grad_tmp)
+        x_prev = np.copy(x)
+        grad_prev = np.copy(grad_tmp)
+        wolfe_cond = wolfe_cond_template(0.001, 0.999, x, f, grad_prev)
+        x += abs(dichotomy(lambda a: f(x + a * p), 0., right_border_calc(lambda a: f(x + a * p)), eps, delt,
+                           is_wolfe=True)) * p
+
+        if store_points:
+            points.append(np.copy(x))
+
+        if np.linalg.norm(x_prev - x) < breaking_eps:
+            break
+
+    if store_points:
+        return points
+    else:
+        return x
